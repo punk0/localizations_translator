7e4f54313b694e44b92d04ff4a787eb5135c8506
nuno <nunomdiniz@gmail.com>
2023-06-12 15:52:07 +0100

    initial push

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:localizations_translator/custom_classes/button_info.dart';
 import 'package:localizations_translator/custom_widgets/buttons/default_button.dart';
+import 'package:localizations_translator/my_shared_preferences.dart';
 import 'package:localizations_translator/my_theme.dart';
 import 'package:localizations_translator/text_frame_original.dart';
 import 'package:localizations_translator/text_frame_translated.dart';
@@ -35,33 +36,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final TextEditingController _apiTextController = TextEditingController();
   String _savedApiKey = '';
-  bool _apiKeySaved = false;
+  bool _keySaved = false;
 
   Future<void> _loadSavedApiKey() async{
+    _savedApiKey = await MySharedPreferences.getGcpApiKey();
 
-
+    setState(() {
     _apiTextController.text = _savedApiKey;
-
+    _keySaved = true;
+    });
   }
   
   Future<void> _saveNewApiKey() async{
-
     setState(() {
       _savedApiKey = _apiTextController.text;
-      _apiKeySaved = true;
+      MySharedPreferences.setGcpApiKey(_savedApiKey);
+      _keySaved = true;
     });
   }
 
-  Future<void> _originalTextChanged(newText) async{
+  Future<void> _apiKeyTextChanged(String newText) async{
+    bool saved = _savedApiKey == _apiTextController.text;
 
+    if (saved != _keySaved)
+      setState(() {
+        _keySaved = saved;
+      });
   }
 
+  Future<void> _originalTextChanged(String newText) async{
+  }
+
+  Future<void> _test() async{
+
+
+
+
+
+
+
+
+  }
 
   @override
   void initState() {
     super.initState();
 
     _loadSavedApiKey();
+
+    _test();
   }
 
   @override
@@ -99,6 +122,7 @@ class _HomePageState extends State<HomePage> {
             buttonInfo: ButtonInfo(
               text: 'SAVE',
               onTap: _saveNewApiKey,
+              enabled: !_keySaved,
             ),
           ),
         ],
@@ -127,15 +151,10 @@ class _HomePageState extends State<HomePage> {
             child: ColoredBox(
               color: MyTheme.BACKGROUND_SEC,
               child: TextFormField(
-                maxLines: 3,
+                maxLines: 1,
                 controller: _apiTextController,
                 style: MyTheme.TEXT_STYLE_BODY,
-                onChanged: (newValue) {
-                  if (_apiKeySaved)
-                    setState(() {
-                      _apiKeySaved = false;
-                    });
-                },
+                onChanged: _apiKeyTextChanged,
               ),
             ),
           )
